45a675f31efd0f80fab11df572e977209d06469d
Daniel Chiquito <daniel.chiquito@gmail.com>
2024-12-03 08:05:51 -0500

    Day 3 part 1

--- cmd/day03/main.go
+++ cmd/day03/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"regexp"
+	"strconv"
 
 	"github.com/dchiquito/advent-of-code-2024/internal/util"
 )
@@ -11,7 +13,16 @@ import (
 func parse(in io.Reader) {}
 
 func level1(in io.Reader) string {
-	return ""
+	input, _ := io.ReadAll(in)
+	re := regexp.MustCompile(`mul\((\d{1,3}),(\d{1,3})\)`)
+	muls := re.FindAllStringSubmatch(string(input), -1)
+	total := 0
+	for _, mul := range muls {
+		left, _ := strconv.Atoi(mul[1])
+		right, _ := strconv.Atoi(mul[2])
+		total += left * right
+	}
+	return fmt.Sprint(total)
 }
 
 func level2(in io.Reader) string {
