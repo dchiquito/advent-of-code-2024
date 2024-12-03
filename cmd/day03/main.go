4d3289972bb75716144dd71dbc3502c6193cfc3b
Daniel Chiquito <daniel.chiquito@gmail.com>
2024-12-03 08:10:45 -0500

    Day 3 part 2

--- cmd/day03/main.go
+++ cmd/day03/main.go
@@ -14,7 +14,7 @@ func parse(in io.Reader) {}
 
 func level1(in io.Reader) string {
 	input, _ := io.ReadAll(in)
-	re := regexp.MustCompile(`mul\((\d{1,3}),(\d{1,3})\)`)
+	re := regexp.MustCompile(`(mul\((\d{1,3}),(\d{1,3})\))|`)
 	muls := re.FindAllStringSubmatch(string(input), -1)
 	total := 0
 	for _, mul := range muls {
@@ -26,7 +26,23 @@ func level1(in io.Reader) string {
 }
 
 func level2(in io.Reader) string {
-	return ""
+	input, _ := io.ReadAll(in)
+	re := regexp.MustCompile(`mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)`)
+	ops := re.FindAllStringSubmatch(string(input), -1)
+	total := 0
+	do := true
+	for _, op := range ops {
+		if op[0] == "do()" {
+			do = true
+		} else if op[0] == "don't()" {
+			do = false
+		} else if do {
+			left, _ := strconv.Atoi(op[1])
+			right, _ := strconv.Atoi(op[2])
+			total += left * right
+		}
+	}
+	return fmt.Sprint(total)
 }
 
 func main() {
