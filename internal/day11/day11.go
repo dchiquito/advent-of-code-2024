7bea5d63ec6a985cccd1882a854b3fa57f9da86a
Daniel Chiquito <daniel.chiquito@gmail.com>
2024-12-11 00:59:00 -0500

    Tuning cache sizes for day 11

--- internal/day11/day11.go
+++ internal/day11/day11.go
@@ -58,35 +58,61 @@ func blinkRepeatedly(cache *[]int, rock int, blinks int) int {
 	return total
 }
 
-func printCache(cache *[]int) {
+func printCache(cache *[]int, maxBlinks int) {
 	for i, c := range *cache {
 		if c > 0 {
-			rock := i / MAX_BLINKS
-			blinks := i % MAX_BLINKS
+			rock := i / maxBlinks
+			blinks := i % maxBlinks
 			fmt.Printf("%d{%d}=%d ", rock, blinks, c)
 		}
 	}
 }
 
 const CACHE_SIZE = 100
-const MAX_BLINKS = 75 // TODO split this between levels
+const MAX_BLINKS = 25
 
 func Level1(in io.Reader) string {
 	seeds := parse(in)
 	cache := make([]int, CACHE_SIZE*MAX_BLINKS)
 	total := 0
 	for _, seed := range seeds {
+		// TODO this throws away a substantial amount of work
+		// blinking thrice at 0 gives (1), (2024), (20, 24)
+		// This approach will cache blinking twice at 1 and blinking once at 2024,
+		// but will not cache blinking once or twice at 0.
+		// Building up a stack might make caching up to twice as efficient?
 		total += blinkRepeatedly(&cache, seed, 25)
 	}
 	return fmt.Sprint(total)
 }
 
+const CACHE_SIZE2 = 1200
+const MAX_BLINKS2 = 75
+
+func blinkRepeatedly2(cache *[]int, rock int, blinks int) int {
+	if blinks == 0 {
+		return 1
+	}
+	if rock < CACHE_SIZE2 && (*cache)[rock*MAX_BLINKS2+blinks] != 0 {
+		return (*cache)[rock*MAX_BLINKS2+blinks]
+	}
+	a, b := blinkOnce(rock)
+	total := blinkRepeatedly2(cache, a, blinks-1)
+	if b != -1 {
+		total += blinkRepeatedly2(cache, b, blinks-1)
+	}
+	if rock < CACHE_SIZE2 {
+		(*cache)[rock*MAX_BLINKS2+blinks] = total
+	}
+	return total
+}
+
 func Level2(in io.Reader) string {
 	seeds := parse(in)
-	cache := make([]int, CACHE_SIZE*MAX_BLINKS)
+	cache := make([]int, CACHE_SIZE2*MAX_BLINKS2)
 	total := 0
 	for _, seed := range seeds {
-		total += blinkRepeatedly(&cache, seed, 75)
+		total += blinkRepeatedly2(&cache, seed, 75)
 	}
 	return fmt.Sprint(total)
 }
