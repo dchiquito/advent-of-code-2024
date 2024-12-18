4e9b97dcf04afc586e4635109065abeb06d68ff3
Daniel Chiquito <daniel.chiquito@gmail.com>
2024-12-18 10:45:38 -0500

    Day 18 part 2

--- internal/day18/day18.go
+++ internal/day18/day18.go
@@ -23,8 +23,7 @@ func parse1(in io.Reader) []bool {
 	return grid
 }
 
-func Level1(in io.Reader) string {
-	grid := parse1(in)
+func shortestPath(grid []bool) int {
 	weights := make([]int, Size*Size)
 	toVisit := []int{0}
 	for len(toVisit) > 0 {
@@ -64,9 +63,44 @@ func Level1(in io.Reader) string {
 			weights[i2] = weights[i] + 1
 		}
 	}
-	return fmt.Sprint(weights[len(weights)-1])
+	return weights[len(weights)-1]
+}
+
+func Level1(in io.Reader) string {
+	grid := parse1(in)
+	solution := shortestPath(grid)
+	return fmt.Sprint(solution)
+}
+
+func parse2(in io.Reader) ([]bool, []int) {
+	scanner := bufio.NewScanner(in)
+	grid := make([]bool, Size*Size)
+	for i := 0; i < 1024; i += 1 {
+		scanner.Scan()
+		line := scanner.Bytes()
+		_, x := util.ChompInt(line, 0)
+		_, y := util.ChompInt(line, 2)
+		grid[(y*Size)+x] = true
+	}
+	additional := []int{}
+	for scanner.Scan() {
+		line := scanner.Bytes()
+		_, x := util.ChompInt(line, 0)
+		_, y := util.ChompInt(line, 2)
+		additional = append(additional, (y*Size)+x)
+	}
+	return grid, additional
 }
 
 func Level2(in io.Reader) string {
+	grid, additional := parse2(in)
+	for _, i := range additional {
+		grid[i] = true
+		if shortestPath(grid) == 0 {
+			x := i % Size
+			y := i / Size
+			return fmt.Sprintf("%d,%d", x, y)
+		}
+	}
 	return ""
 }
