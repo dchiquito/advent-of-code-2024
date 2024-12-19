3bc16181fcf167cd940a5cf1150a7220ffe94f0f
Daniel Chiquito <daniel.chiquito@gmail.com>
2024-12-19 09:23:17 -0500

    Day 19 part 1

--- internal/day19/day19.go
+++ internal/day19/day19.go
@@ -1,13 +1,47 @@
 package day19
 
 import (
+	"bufio"
+	"fmt"
 	"io"
+	"strings"
 )
 
-func parse(in io.Reader) {}
+func parse(in io.Reader) ([]string, []string) {
+	scanner := bufio.NewScanner(in)
+	scanner.Scan()
+	towels := strings.Split(scanner.Text(), ", ")
+	scanner.Scan()
+	patterns := []string{}
+	for scanner.Scan() {
+		patterns = append(patterns, scanner.Text())
+	}
+	return towels, patterns
+}
+
+func recur(towels []string, pattern string) bool {
+	if len(pattern) == 0 {
+		return true
+	}
+	for _, towel := range towels {
+		if len(pattern) >= len(towel) && pattern[:len(towel)] == towel {
+			if recur(towels, pattern[len(towel):]) {
+				return true
+			}
+		}
+	}
+	return false
+}
 
 func Level1(in io.Reader) string {
-	return ""
+	towels, patterns := parse(in)
+	total := 0
+	for _, pattern := range patterns {
+		if recur(towels, pattern) {
+			total += 1
+		}
+	}
+	return fmt.Sprint(total)
 }
 
 func Level2(in io.Reader) string {
