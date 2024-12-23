e2770772267df9c8cfc9a033d0f8a96a881e123a
Daniel Chiquito <daniel.chiquito@gmail.com>
2024-12-23 12:13:34 -0500

    Day 23 part 2

--- internal/day23/day23.go
+++ internal/day23/day23.go
@@ -68,6 +68,77 @@ func Level1(in io.Reader) string {
 	return fmt.Sprint(total)
 }
 
+func largestSubgroup(graph Graph, n int) int {
+	adjs := graph[n]
+	notIncluded := make([]bool, len(adjs))
+	numNotIncluded := 0
+	var clumps []int
+	for numNotIncluded < len(adjs) {
+		clumps = make([]int, len(adjs))
+		for i, ni := range adjs {
+			if notIncluded[i] {
+				continue
+			}
+			for j := i + 1; j < len(adjs); j += 1 {
+				if notIncluded[j] {
+					continue
+				}
+				nj := adjs[j]
+				if isAdjacent(graph, ni, nj) {
+					clumps[i] += 1
+					clumps[j] += 1
+				}
+			}
+		}
+		smallest := 9999
+		for i, c := range clumps {
+			if !notIncluded[i] && c < smallest {
+				smallest = c
+			}
+		}
+		for i, c := range clumps {
+			if !notIncluded[i] && c == smallest {
+				notIncluded[i] = true
+				numNotIncluded += 1
+			}
+		}
+	}
+	xxx := 1
+	for _, c := range clumps {
+		if c > 0 {
+			xxx += 1
+		}
+	}
+	return xxx
+}
+
 func Level2(in io.Reader) string {
-	return ""
+	graph := parse(in)
+	// Every node has 13 neighbors
+	// The largest possible connected subgroup is 14
+	largestGroupSize := 0
+	var group []int
+	for n := range graph {
+		if graph[n] != nil {
+			groupSize := largestSubgroup(graph, n)
+			if groupSize > largestGroupSize {
+				group = make([]int, 0, 14)
+				largestGroupSize = groupSize
+			}
+			if groupSize == largestGroupSize {
+				group = append(group, n)
+			}
+		}
+	}
+	// Conveniently, the list of nodes is already sorted
+	// Build the string from the list of nodes
+	arr := make([]byte, len(group)*3-1)
+	arr[0] = byte(group[0]/26) + 'a'
+	arr[1] = byte(group[0]%26) + 'a'
+	for i := 1; i < len(group); i += 1 {
+		arr[i*3-1] = ','
+		arr[i*3] = byte(group[i]/26) + 'a'
+		arr[i*3+1] = byte(group[i]%26) + 'a'
+	}
+	return string(arr)
 }
