a2045c4f2aabd1116bf3d6f7a3feb50ff698a6cb
Daniel Chiquito <daniel.chiquito@gmail.com>
2024-12-23 11:25:53 -0500

    Day 23 part 1

--- internal/day23/day23.go
+++ internal/day23/day23.go
@@ -1,13 +1,86 @@
 package day23
 
 import (
+	"bufio"
+	"fmt"
 	"io"
 )
 
-func parse(in io.Reader) {}
+type Graph [][]int
+
+func parse(in io.Reader) Graph {
+	scanner := bufio.NewScanner(in)
+	graph := make(Graph, 26*26)
+	for scanner.Scan() {
+		line := scanner.Bytes()
+		a := int(line[0] - 'a')
+		b := int(line[1] - 'a')
+		c := int(line[3] - 'a')
+		d := int(line[4] - 'a')
+		left := a*26 + b
+		right := c*26 + d
+		if graph[left] == nil {
+			graph[left] = []int{}
+		}
+		graph[left] = append(graph[left], right)
+		if graph[right] == nil {
+			graph[right] = []int{}
+		}
+		graph[right] = append(graph[right], left)
+	}
+	return graph
+}
+
+func isAdjacent(graph Graph, n1 int, n2 int) bool {
+	for _, adj := range graph[n1] {
+		if adj == n2 {
+			return true
+		}
+	}
+	return false
+}
 
 func Level1(in io.Reader) string {
-	return ""
+	graph := parse(in)
+	total := 0
+	for i := 26 * 19; i < 26*20; i += 1 {
+		if graph[i] != nil {
+			for j := 0; j < len(graph[i]); j += 1 {
+				nj := graph[i][j]
+				if nj/26 == 19 && nj < i {
+					// Avoid double counting
+					continue
+				}
+				for k := j + 1; k < len(graph[i]); k += 1 {
+					nk := graph[i][k]
+					if nk/26 == 19 && nk < i {
+						// Avoid double counting
+						continue
+					}
+					// check if graph[i][j] is in graph[i][k]
+					for _, nkk := range graph[nk] {
+						if nkk == nj {
+							// it dooo
+							total += 1
+							break
+						}
+					}
+				}
+			}
+		}
+	}
+	// for i := 26 * 19; i < 26*20; i += 1 {
+	// 	for j := 0; j < len(graph); j += 1 {
+	// 		if isAdjacent(graph, i, j) {
+	// 			for k := 0; k < len(graph); k += 1 {
+	// 				if isAdjacent(graph, i, k) && isAdjacent(graph, j, k) {
+	// 					total += 1
+	// 				}
+	// 			}
+	// 		}
+	// 	}
+	// }
+	return fmt.Sprint(total)
 }
 
 func Level2(in io.Reader) string {
